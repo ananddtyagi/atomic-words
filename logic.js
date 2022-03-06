b37380aa02d76dabff47e22659880181e754217c
Anand Tyagi <anand.deep.tyagi@gmail.com>
2022-03-06 15:19:45 -0800

    initial page

---
+++ logic.js
@@ -0,0 +1,7 @@
+function generateElementalBreakdown() {
+  var word = document.getElementById("word");
+  var elementalBreakdown = document.getElementById("elementalBreakdown");
+  var output = "<p>No elemental breakdown found</p>";
+
+  elementalBreakdown.innerHTML = output;
+}
